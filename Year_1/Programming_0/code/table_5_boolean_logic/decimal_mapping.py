08a7c8e24388bd123ffdcfc7574806effba56d26
tookiebunten <davidtookiebunten@gmail.com>
2024-10-28 15:59:06 +0000

    Add decimal_mapping.py for mapping numeric values on a Mac keyboard

---
+++ Year_1/Programming_0/code/table_5_boolean_logic/decimal_mapping.py
@@ -0,0 +1,7 @@
+"""The numeric values on a Mac keyboard look like this: 
+ 
+If the user holds the shift key while pressing key ‘6’ the symbol ‘^’ is produced. 
+Assuming that the user either holds the shift key or doesn’t, while pressing an 
+arbitrary number of keys, create a function that receives the resulting list of key 
+presses, as numeric values, and the boolean state of the shift key (is_pressed), 
+then returns from the function the appropriate mapping from the list of numbers. """
