4d8a6124b1db942ccae9d6d1c82a92c22578c7f8
tookiebunten <davidtookiebunten@gmail.com>
2024-10-28 15:55:25 +0000

    Add function to convert text to numerical value based on if else statements

--- Year_1/Programming_0/code/table_4_boolean_logic/text_n_power.py
+++ Year_1/Programming_0/code/table_4_boolean_logic/text_n_power.py
@@ -1,1 +1,7 @@
-
+"""Consider a call to a script: 
+ 
+text_n_power(“three”, 2) 
+ 
+With if else statements, and covering command line 
+input cases for stings {“one,” “two”, “three”} and 
+integers {1, 2, 3, 4, 5} return the numerical value. """
--- Year_1/Programming_0/code/table_4_boolean_logic/tired_hungry_thirsty.py
+++ Year_1/Programming_0/code/table_4_boolean_logic/tired_hungry_thirsty.py
@@ -1,1 +1,18 @@
+"""Consider three separate states – tired, hungry, thirsty. 
+A table is provided, below. It is a mapping between 
+states to an associated action.  
+ 
+Create a function that takes three arguments, which 
+are the states, and returns the corresponding action. 
 
+ired |hungry |thirsty |action 
+False |False |False |“do nothing” 
+False |False |True |“drink” 
+False |True |False |“eat” 
+False |True |True |“drink and eat” 
+True |False |False |“sleep” 
+True |False |True |“drink in bed” 
+True |True |False |“eat in bed” 
+True |True |True |“eat and drink in bed” 
+
+"""
