33529a9177044cb611182b41537a640c4f1f338d
tookiebunten <davidtookiebunten@gmail.com>
2024-11-02 13:24:42 +0000

    Add calculate_expression function with test cases for validation

--- Year_1/Programming_0/code/table_3_arithmetic/precedence.py
+++ Year_1/Programming_0/code/table_3_arithmetic/precedence.py
@@ -1,6 +1,20 @@
-"""Receives 7 parameters a to g, which are arranged according to the 
-following equation: 
- 
- ((((a +b)* c * d)/(e - f + g)) / f ) -g
- 
-Return the result of the calculation."""
+def calculate_expression(a, b, c, d, e, f, g) -> float:
+    """Receives 7 parameters a to g, which are arranged according to the 
+    following equation: 
+    
+    ((((a + b) * c * d) / (e - f + g)) / f) - g
+    
+    Return the result of the calculation."""
+    
+    result = ((((a + b) * c * d) / (e - f + g)) / f) - g
+    return result
+
+def test_calculate_expression() -> None:
+    assert calculate_expression(1, 2, 3, 4, 5, 6, 7) == -6
+    assert calculate_expression(1, 1, 1, 1, 1, 1, 1) == 1
+    assert calculate_expression(10, 20, 30, 40, 50, 60, 70) == -60
+    print("All tests passed.")
+
+if __name__ == "__main__":
+    print("Result of calculate_expression for (1, 2, 3, 4, 5, 6, 7):", calculate_expression(1, 2, 3, 4, 5, 6, 7))
+    test_calculate_expression()
